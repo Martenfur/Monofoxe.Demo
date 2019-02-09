08bebb96b0ef99d59c53a9f84bfd3d4b65aeeda7
gn.fur <patych7@gmail.com>
2019-02-10 01:16:39 +0300

    Previous commit fix.

---
+++ Monofoxe.Demo/Monofoxe.Demo/GameLogic/Tiles/ColliderTilesetTile.cs
@@ -0,0 +1,32 @@
+﻿using Monofoxe.Engine.Drawing;
+using Monofoxe.Engine.Utils.Tilemaps;
+using Monofoxe.Demo.GameLogic.Collisions;
+using Microsoft.Xna.Framework;
+
+namespace Monofoxe.Demo.GameLogic.Tiles
+{
+	public class ColliderTilesetTile : ITilesetTile
+	{
+		public Frame Frame {get; private set;}
+		public ICollider Collider {get; private set;}
+		public TilesetTileCollisionType CollisionType {get; private set;}
+		public Vector2 ColliderOffset;
+		
+
+		public ColliderTilesetTile(Frame frame, ICollider collider, TilesetTileCollisionType collisionType, Vector2 colliderOffset)
+		{
+			Frame = frame;
+			Collider = collider;
+			CollisionType = collisionType;
+			ColliderOffset = colliderOffset;
+		}
+	}
+
+	public enum TilesetTileCollisionType
+	{
+		None = 0, // Tile isn't solid.
+		Solid = 1, // Tile is solid.
+		Custom = 2, // Tile uses custom collider.
+	}
+
+}
